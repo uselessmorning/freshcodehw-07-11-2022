ef08b6ddc30e0bf6cdd831dcf47096b36e899096
uselessmorning <thebigtonny3@gmail.com>
2022-11-07 10:04:12 +0200

    add new file .gitignore

---
+++ index.js
@@ -0,0 +1,8 @@
+let howoldareman = 23;
+console.log (howoldareman)
+let isheadult = true;
+console.log (isheadult)
+let ProgrammingLanguage = 'C++';
+console.log (ProgrammingLanguage)
+let IsPeopleHaveAnimal = 'no';
+console.log (IsPeopleHaveAnimal)
